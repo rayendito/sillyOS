adea94a8526cd21582f4b36327735715346ee2ca
rayendito <13519157@std.stei.itb.ac.id>
2024-08-12 22:31:10 +0700

    comments explaining how the printing functions works

--- printing.asm
+++ printing.asm
@@ -4,12 +4,12 @@ print_string:
     mov ah, 0x0e ; tele something mode
 
     loop_ps:
-        cmp byte [bx], 0 ; if byte is 0, end loop
+        cmp byte [bx], 0 ; if byte value in address pointed by bx is 0, end loop. [] is dereference
         je endloop_ps
-        mov al, [bx] ; prepare the interrupt
-        int 0x10 ; interrupt to print
-        add bx, 1 ; increment to the next address
-        jmp loop_ps
+        mov al, [bx] ; al is where the ascii byte to print should go. and it's pointed to by bx
+        int 0x10 ; calling the interrupt
+        add bx, 1 ; increment bx to point to the next address
+        jmp loop_ps ; back to loop_ps
     
     endloop_ps:
     popa
@@ -17,7 +17,7 @@ print_string:
 
 print_newline:
     pusha
-        mov ah, 0x0e
+        mov ah, 0x0e    ; preparing the interrupt. entering tele-something mode
         mov al, 0x0d    ; ASCII code for carriage return
         int 0x10        ; Call BIOS interrupt
         mov al, 0x0a    ; ASCII code for line feed
@@ -28,37 +28,35 @@ print_newline:
 ; put hex in dx
 print_hex:
     pusha
-        ; move in the hex string
-        ; start after the 0x
-        mov bx, HEX_OUT
-        add bx, 2
+        mov bx, HEX_OUT ; move in the default hex string 0x0000
+        add bx, 2       ; start after the 0x
 
         ; change the contents of bx
         loop_ph:
-            cmp word dx, 0
+            cmp word dx, 0  ; if dx is completely shifted, done
             je endloop_ph
             
-            mov cx, dx
-            and cx, 0xf000
-            shr cx, 12
+            mov cx, dx      ; cx as a temporary variable
+            and cx, 0xf000  ; get the LSB only by masking
+            shr cx, 12      ; get this LSB to MSB
             
-            cmp cx, 9
+            cmp cx, 9       ; if it's a number, add with 0x30 to get the right ascii char
             jle ascii_digit
-            add cx, 0x37 ; turn to ascii value of A-F
+            add cx, 0x37    ; if it's A-F, add with 0x37 to get the right ascii char
             jmp change_char
             ascii_digit:
             add cx, 0x30
             
             change_char:
-            mov word [bx], cx
-            shl dx, 4
-            add bx, 1
+            mov word [bx], cx   ; changing the value pointed to by bx to the ascii character
+            shl dx, 4           ; get the next char to print
+            add bx, 1           ; get the next address to store the char
             jmp loop_ph
         endloop_ph:
 
         ; returning to the front
-        mov bx, HEX_OUT
-        call print_string
+        mov bx, HEX_OUT         ; return to the original first address of the dummy string
+        call print_string       ; calling print string
     popa
     ret
 
