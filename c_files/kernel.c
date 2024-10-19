6103d5d59072e95ba1e4e5549a98534eee4f7fbd
rayendito <13519157@std.stei.itb.ac.id>
2024-10-19 22:23:28 +0700

    initial commit for kernel c code

---
+++ c_files/kernel.c
@@ -0,0 +1,21 @@
+// compiled and linked on an M2 chip using cross compiler and linker
+// compiling
+// x86_64-elf-gcc c_files/kernel.c -c -o c_files/kernel.o
+// linking
+// x86_64-elf-ld -o bin/kernel.bin -Ttext 0x1000 c_files/kernel.o --oformat binary
+// ========================================================================
+// note on -Ttext
+// basically this specifies the offset of this code
+// exactly like [org 0x7c00] for boot code
+// but we're putting our kernel instructions in 0x1000
+// if you look at page 14 it shows that 0x1000 is a free space in the memory after boot
+// i think we can set this to anywhere? (unlike boot code which has to be on 0x7c00)
+// why 0x7c00? https://f.osdev.org/viewtopic.php?t=24129
+
+void main() {
+    // where video memory resides
+    char* video_memory = (char*) 0xb8000;
+
+    // add something to the video memory
+    *video_memory = 'P';
+}
